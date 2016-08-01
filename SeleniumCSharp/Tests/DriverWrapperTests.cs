cf9dbfba894e8a1662827eef593b9f5460ff95f3
Selvaraj, Prasanna <VSELVPR@bitbucket.pearson.com>
2016-08-01 12:48:50 +0530

    Updated test to add setup

--- SeleniumCSharp/Tests/DriverWrapperTests.cs
+++ SeleniumCSharp/Tests/DriverWrapperTests.cs
@@ -7,25 +7,34 @@ using NUnit.Framework;
 using SeleniumCSharp.Selenium;
 using OpenQA.Selenium.Firefox;
 using SeleniumCSharp.Framework;
-
+using WebServiceCSharp.Core;
 namespace SeleniumCSharp.Tests
 {
 
     public class DriverWrapperTests
     {
 
+        [SetUp]
+        public void Setup()
+        {
+            Logger.logWriter = TestContext.Out;
+            Logger.name = TestContext.CurrentContext.Test.FullName;
+            Logger.mode = LogMode.INFO;
+        }
+
         [Test]
         public void GotoURLTest()
         {
-
+            Logger.Info("Starting Test");
             DriverWrapper wrapper = DriverUtils.GetDriver();
 
             wrapper.Navigate().GoToUrl("http://www.carnaticcorner.com/library.html");
 
-
             DriverWrapper wrapper1 = DriverUtils.GetDriver();
 
             wrapper1.Navigate().GoToUrl("http://www.carnaticcorner.com/library.html");
+            Logger.Info("Finished test");
+
         }
 
 
