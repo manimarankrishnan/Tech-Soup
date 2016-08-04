9a24b9ac945ba30d43138aa7a42884679ea3e76e
Selvaraj, Prasanna <VSELVPR@bitbucket.pearson.com>
2016-08-04 17:28:13 +0530

    Added DriverCommands

--- SeleniumCSharp/Selenium/DriverCommands.cs
+++ SeleniumCSharp/Selenium/DriverCommands.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WebServiceCSharp.Core;
+using System.Drawing.Imaging;
 namespace SeleniumCSharp.Selenium
 {
     public class DriverCommands
     {
+
+        public DriverCommands(DriverWrapper driver)
+        {
+            Driver = driver;
+        }
+
+        public DriverWrapper Driver { get; set; }
+
+        public void SaveScreenshot()
+        {
+            try
+            {
+                String fileName = Logger.GetFilePath() +Utils.GetUniqueNumber() + ".jpg";
+                Driver.GetScreenshot().SaveAsFile(fileName, ImageFormat.Jpeg);
+                Logger.Debug("Screenshot saved at  <a href=\"file://{0}\">{0}</a>", fileName);
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+            }
+        }
+
+        public void SavePageSource()
+        {
+            try
+            {
+                String fileName =Logger.GetFilePath()+Utils.GetUniqueNumber()+ ".html";
+                using (var writer = new System.IO.StreamWriter(fileName))
+                {
+                    writer.Write(Driver.PageSource);
+                    writer.Close();
+                }
+                Logger.Debug("HTML Page Source Saved at <a href=\"file://{0}\">{0}</a>", fileName);
+            }
+            catch (Exception e)
+            {
+                Logger.Error(e);
+            }
+        }
+
+
     }
 }
